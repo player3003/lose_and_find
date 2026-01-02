e2fe25bd2b4f1e35f31ae26d566e4871a56f5bf8
player3003 <zhuangchengfeng46@gmail.com>
2026-01-02 15:57:11 +0800

    添加 User 模型到导出列表

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from models.item import FoundItem, LostItem
 from models.match_result import MatchLevel, MatchResult
+from models.user import User
 
 __all__ = [
     "LostItem",
     "FoundItem",
     "MatchResult",
     "MatchLevel",
+    "User",
 ]
